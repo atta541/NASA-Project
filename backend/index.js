0b9e7193854239cfa3a8d29aad8e64d702aaa48a
atta541 <attareh542@gmail.com>
2025-07-13 20:17:55 +0500

    fix the cors origin issue

--- backend/index.js
+++ backend/index.js
@@ -6,31 +6,28 @@ const techTransferRoutes = require('./src/routes/techtransfer.routes');
 const neoRoutes = require('./src/routes/neo.routes');
 const marsRoutes = require('./src/routes/mars.routes');
 
-
-
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors()); 
-app.use(express.json());
+const corsOptions = {
+  origin: process.env.REACT_APP_FRONTEND_URL, 
+  optionsSuccessStatus: 200 
+};
 
+
+app.use(cors(corsOptions)); 
+app.use(express.json());
 
 app.use('/apod', apodRoutes);
 app.use('/techtransfer', techTransferRoutes);
 app.use('/neo', neoRoutes);
 app.use('/mars', marsRoutes);
 
-
-
-
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong!' });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
